65a2bcf5e0365a6ac10a955a94b51625e7433542
冬馬せつな <89218785+zero6go@users.noreply.github.com>
2024-01-22 15:02:31 +0800

    1

---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "Zero"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Zero"
+	location "Zero"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.22000.0"
+
+		defines
+		{
+			"ZERO_PLATFORM_WINDOWS",
+			"ZERO_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/Sandbox/\"")
+		}
+
+	filter "configurations:Debug"
+		defines "ZERO_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "ZERO_RELEASE"
+		symbols "On"
+
+	filter "configurations:Dist"
+		defines "ZERO_DIST"
+		symbols "On"
+
+project "SandBox"
+	location "SandBox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Zero/vendor/spdlog/include",
+		"Zero/src"
+	}
+
+	links
+	{
+		"Zero"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.22000.0"
+
+		defines
+		{
+			"ZERO_PLATFORM_WINDOWS",
+		}
+
+	filter "configurations:Debug"
+		defines "ZERO_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "ZERO_RELEASE"
+		symbols "On"
+
+	filter "configurations:Dist"
+		defines "ZERO_DIST"
+		symbols "On"
